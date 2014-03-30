650320451cb2cb713d733382a076eeb980819a21
Niklas Hambüchen <mail@nh2.me>
2014-03-30 07:57:07 +0100

    Prints

--- src/main/java/hudson/plugins/git/util/GitUtils.java
+++ src/main/java/hudson/plugins/git/util/GitUtils.java
@@ -177,7 +177,11 @@ public class GitUtils implements Serializable {
                         LOGGER.fine(MessageFormat.format(
                                 "Computed merge bases in {0} commit steps and {1} ms", calls,
                                 (System.currentTimeMillis() - start)));
+                        System.out.println(MessageFormat.format(
+                                "\n\Computed merge bases in {0} commit steps and {1} ms\n", calls,
+                                (System.currentTimeMillis() - start)));
 
+                    System.out.println("size " + tipCandidates.values().size());
                     return new ArrayList<Revision>(tipCandidates.values());
                 }
             });
